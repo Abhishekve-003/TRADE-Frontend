edd421df84b805aad69933aa619cae1abd55f7f0
Abhishek <abhishek@beanbyte.dev>
2024-04-06 13:08:30 +0530

    Your commit message here

--- src/components/BtcUSD.js
+++ src/components/BtcUSD.js
@@ -1,66 +1,75 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import CanvasJSReact from '@canvasjs/react-charts';
 import axios from 'axios';
+import { Button, CircularProgress } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { Button } from '@mui/material';
 import { server } from '../App';
 
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-function BtcUSD() {
-    const [chartData, setChartData] = useState([]);
+function BTCUSD() {
+    const [symbol, setSymbol] = useState('BTCUSD'); 
+  const [chartData, setChartData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData(symbol);
+  }, [symbol]);
 
-    const fetchData = async () => {
-        try {
-            const response = await axios.get(`${server}/BTCUSD`);
-            const data = response.data;
-            const processedData = data.map(item => ({
-                x: new Date(item.fullDate),
-                y: [item.open, item.high, item.low, item.close]
-            }));
-            setChartData(processedData);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
+  const fetchData = async (symbol) => {
+    try {
+      setLoading(true);
+      const response = await axios.get(`${server}/${symbol}`);
+      const data = response.data;
+      const processedData = data.map(item => ({
+        x: new Date(item.fullDate),
+        y: [item.open, item.high, item.low, item.close]
+      }));
+      setChartData(processedData);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setLoading(false);
+    }
+  };
 
-    const options = {
-        theme: 'light2',
-        animationEnabled: true,
-        exportEnabled: true,
-        zoomEnabled: true, 
+
+const handleSymbolChange = (selectedSymbol) => {
+    setSymbol(selectedSymbol);
+};
+
+  const options = useMemo(() => ({
+    theme: 'light2',
+    animationEnabled: true,
+    exportEnabled: true,
+    zoomEnabled: true, 
     zoomType: 'xy', 
-        title: {
-            text: 'BTCUSD Candlestick Chart'
-        },
-        axisX: {
-            valueFormatString: 'MMM DD YYYY'
-        },
-        axisY: {
-            title: 'Price'
-        },
-        data: [
-            {
-                type: 'candlestick',
-                dataPoints: chartData
-            }
-        ]
-    };
+    title: {
+      text: `${symbol} Candlestick Chart`
+    },
+    axisX: {
+      valueFormatString: 'MMM DD YYYY'
+    },
+    axisY: {
+      title: 'Price'
+    },
+    data: [
+      {
+        type: 'candlestick',
+        dataPoints: chartData
+      }
+    ]
+  }), [chartData]);
 
-    return (
-        <div>
-            <CanvasJSChart options={options} />
-            <Link to="/">
-                <Button size='small' variant='contained' sx={{ borderRadius: '12px' }}>
-                    EURUSD
-                </Button>
-            </Link>
-        </div>
-    );
+  return (
+    <div>
+        <Link to="/">
+                 <Button onClick={() => handleSymbolChange('EURUSD')}>Go To EURUSD</Button>
+        </Link>
+      {/* <Button onClick={() => handleSymbolChange('BTCUSD')}>BTCUSD</Button> */}
+      {loading ? <CircularProgress /> : <CanvasJSChart options={options} />}
+    </div>
+  );
 }
 
-export default BtcUSD;
+export default BTCUSD;
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,136 +1,80 @@
 
-// import React, { useState, useEffect } from 'react';
-// import CanvasJSReact from '@canvasjs/react-charts';
-// import axios from 'axios';
-// import { Button } from '@mui/material';
-// import { Link } from 'react-router-dom';
-
-// var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-
-// function Dashboard() {
-//   const [symbol, setSymbol] = useState('EURUSD'); 
-//   const [chartData, setChartData] = useState([]);
-
-//   useEffect(() => {
-//     fetchData(symbol);
-//   }, [symbol]);
-
-//   const fetchData = async (symbol) => {
-//     try {
-//       const response = await axios.get(`http://localhost:4000/api/data/${symbol}`);
-//       const data = response.data;
-//       const processedData = data.map(item => ({
-//         x: new Date(item.fullDate),
-//         y: [item.open, item.high, item.low, item.close]
-//       }));
-//       setChartData(processedData);
-//     } catch (error) {
-//       console.error('Error fetching data:', error);
-//     }
-//   };
-
-//   const options = {
-//     theme: 'light2',
-//     animationEnabled: true,
-//     exportEnabled: true,
-//     title: {
-//       text: `${symbol} Candlestick Chart`
-//     },
-//     axisX: {
-//       valueFormatString: 'MMM DD YYYY'
-//     },
-//     axisY: {
-//       title: 'Price'
-//     },
-//     data: [
-//       {
-//         type: 'candlestick',
-//         dataPoints: chartData
-//       }
-//     ]
-//   };
-
-//   return (
-//     <div>
-//       <CanvasJSChart options={options} />
-//       <Link to="/btcusd">
-//         <Button size='small' variant='contained' sx={{ borderRadius: '12px' }}>
-//           BTCUSD
-//         </Button>
-//       </Link>
-//     </div>
-//   );
-// }
-
-// export default Dashboard;
-
-
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import CanvasJSReact from '@canvasjs/react-charts';
 import axios from 'axios';
-import { Button } from '@mui/material';
+import { Button, CircularProgress } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { server } from '../App';
 
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 function Dashboard() {
-  const [symbol, setSymbol] = useState('EURUSD'); 
-  const [chartData, setChartData] = useState([]);
+    const [symbol, setSymbol] = useState('EURUSD'); 
+    const [chartData, setChartData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    fetchData(symbol);
-  }, [symbol]);
+    useEffect(() => {
+        fetchData(symbol);
+    }, [symbol]);
 
-  const fetchData = async (symbol) => {
-    try {
-      // const response = await axios.get(`http://localhost:4000/api/data/${symbol}`);
-      const response = await axios.get(`${server}/${symbol}`);
-      const data = response.data;
-      const processedData = data.map(item => ({
-        x: new Date(item.fullDate),
-        y: [item.open, item.high, item.low, item.close]
-      }));
-      setChartData(processedData);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
+    const fetchData = async (symbol) => {
+        try {
+            setIsLoading(true);
+            const response = await axios.get(`${server}/${symbol}`);
+            const data = response.data;
+            const processedData = data.map(item => ({
+                x: new Date(item.fullDate),
+                y: [item.open, item.high, item.low, item.close]
+            }));
+            setChartData(processedData);
+            setIsLoading(false);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            setIsLoading(false);
+        }
+    };
 
-  const options = {
-    theme: 'light2',
-    animationEnabled: true,
-    exportEnabled: true,
-    zoomEnabled: true, 
-    zoomType: 'xy', 
-    title: {
-      text: `${symbol} Candlestick Chart`
-    },
-    axisX: {
-      valueFormatString: 'MMM DD YYYY'
-    },
-    axisY: {
-      title: 'Price'
-    },
-    data: [
-      {
-        type: 'candlestick',
-        dataPoints: chartData
-      }
-    ]
-  };
+    const handleSymbolChange = (selectedSymbol) => {
+        setSymbol(selectedSymbol);
+    };
 
-  return (
-    <div>
-      <CanvasJSChart options={options} />
-      <Link to="/btcusd">
-        <Button size='small' variant='contained' sx={{ borderRadius: '12px' }}>
-          BTCUSD
-        </Button>
-      </Link>
-    </div>
-  );
+    const options = useMemo(() => ({
+        theme: 'light2',
+        animationEnabled: true,
+        exportEnabled: true,
+        zoomEnabled: true, 
+        zoomType: 'xy', 
+        title: {
+            text: `${symbol} Candlestick Chart`
+        },
+        axisX: {
+            valueFormatString: 'MMM DD YYYY'
+        },
+        axisY: {
+            title: 'Price'
+        },
+        data: [
+            {
+                type: 'candlestick',
+                dataPoints: chartData
+            }
+        ]
+    }), [symbol, chartData]);
+
+    return (
+        <div>
+          <Link to="/btcusd">
+               <Button onClick={() => handleSymbolChange('BTCUSD')}>Go To BTCUSD</Button>
+          </Link>
+            
+            {/* <Button onClick={() => handleSymbolChange('EURUSD')}>EURUSD</Button> */}
+            {isLoading ? (
+                <CircularProgress />
+            ) : (
+                <CanvasJSChart options={options} />
+            )}
+        </div>
+    );
 }
 
 export default Dashboard;
-
